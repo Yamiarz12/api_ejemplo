11626e966e73bb4af9744545254677cc4d40f5fa
Yamileth Ruiz <yamiruiz044@gmail.com>
2022-11-18 19:50:47 -0600

    feat: initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import taskRouter from "./controllers/task-controller";
+
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/tasks", taskRouter);
+
+app.listen(3000, () =>{
+
+console.log("Running on port 3000");
+    });
